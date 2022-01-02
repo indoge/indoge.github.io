9e93355184b12872d17e6e60e42ebddd6578ab28
Chandru Kumar R <indiandoge@gmail.com>
2022-01-02 15:06:01 +0530

    Added virtual scroll for tweets - but not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +8,7 @@ import { LatestDropsComponent } from './latest-drops/latest-drops.component';
 import { HomeComponent } from './home/home.component';
 import { RewardsprogramComponent } from './rewardsprogram/rewardsprogram.component';
 import { ElonDogeTweetsComponent } from './elon-doge-tweets/elon-doge-tweets.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { ElonDogeTweetsComponent } from './elon-doge-tweets/elon-doge-tweets.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ScrollingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
