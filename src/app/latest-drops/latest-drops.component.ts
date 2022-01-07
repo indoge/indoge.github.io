5abeaf5f7b2c1b93a3436898d4714f8def4af908
Chandru Kumar R <indiandoge@gmail.com>
2022-01-07 14:44:28 +0530

    Added more in latest drops

--- src/app/latest-drops/latest-drops.component.ts
+++ src/app/latest-drops/latest-drops.component.ts
@@ -9,6 +9,66 @@ export class LatestDropsComponent implements OnInit {
 
   latestDrops = [
     {
+      imgPath : '../assets/Metaverse - Common.png',
+      title : 'Metaverse Doge',
+      rarity : 'Common'
+    },
+    {
+      imgPath : '../assets/Metaverse - Rare.png',
+      title : 'Metaverse Doge',
+      rarity : 'Rare'
+    },
+    {
+      imgPath : '../assets/Metaverse - Epic.png',
+      title : 'Metaverse Doge',
+      rarity : 'Epic'
+    },
+    {
+      imgPath : '../assets/Red suit - Common.png',
+      title : 'Red suit Doge',
+      rarity : 'Common'
+    },
+    {
+      imgPath : '../assets/Red suit - Rare.png',
+      title : 'Red suit Doge',
+      rarity : 'Rare'
+    },
+    {
+      imgPath : '../assets/Red suit - Epic.png',
+      title : 'Red suit Doge',
+      rarity : 'Epic'
+    },
+    {
+      imgPath : '../assets/StarGlass - Common.png',
+      title : 'StarGlass Doge',
+      rarity : 'Common'
+    },
+    {
+      imgPath : '../assets/StarGlass - Rare.png',
+      title : 'StarGlass Doge',
+      rarity : 'Rare'
+    },
+    {
+      imgPath : '../assets/StarGlass - Epic.png',
+      title : 'StarGlass Doge',
+      rarity : 'Epic'
+    },
+    {
+      imgPath : '../assets/DogeInLoveGlass - Common.png',
+      title : 'Doge In Love',
+      rarity : 'Common' 
+    },
+    {
+      imgPath : '../assets/DogeInLoveGlass - Rare.png',
+      title : 'Doge In Love',
+      rarity : 'Rare'
+    },
+    {
+      imgPath : '../assets/DogeInLoveGlass - Epic.png',
+      title : 'Doge In Love',
+      rarity : 'Epic'
+    },
+    {
       imgPath : '../assets/Sleeveless - Common.png',
       title : 'Sleeveless Doge',
       rarity : 'Common'
